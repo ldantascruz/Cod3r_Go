07141fe9f23da88d88b7c9fe026dc099fe3cf7bd
Lucas Cruz <ldantascruz@gmail.com>
2025-01-25 13:47:07 -0300

    inicio módulo tipos

---
+++ tipos/interface1/interface1.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type imprimivel interface {
+	toString() string
+}
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+}
+
+type produto struct {
+	nome  string
+	preco float64
+}
+
+// interfaces são implementadas implicitamente
+func (p pessoa) toString() string {
+	return p.nome + " " + p.sobrenome
+}
+
+func (p produto) toString() string {
+	return fmt.Sprintf("%s - R$%.2f\n", p.nome, p.preco)
+}
+
+func imprimir(x imprimivel) {
+	fmt.Println(x.toString())
+}
+
+func main() {
+
+}
